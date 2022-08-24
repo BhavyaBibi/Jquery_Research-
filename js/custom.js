0d2177d79e43d65e500fb0f1740f3f62f2fda5a3
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-24 14:21:34 +0400

    cfolor code setting is done

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     $("#test").html("Heloow How are you ! ")
     $("input[type='checkbox']  ").prop("checked", false)
     $('#username').attr("type", "password")
-    $('a').css("color", "red")
-    $('a').css("background-color", "green")
-})  
+    //     $('a').css("color", "red")
+    //     $('a').css("background-color", "green")
+    // })  
+
+    //we can set the background color and color 
+    $('a').css({ "color": "white", "background-color": "green" })
+})
