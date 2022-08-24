aacc665d07b3e1645e25f6932e0d013f17382e4c
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-24 14:09:38 +0400

    the changes added in the code section

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,6 @@ $(document).ready(function () {
     $("#test").html("Heloow How are you ! ")
     $("input[type='checkbox']  ").prop("checked", false)
     $('#username').attr("type", "password")
-})
+    $('a').css("color", "red")
+    $('a').css("background-color", "green")
+})  
