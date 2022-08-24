d1ff0dd46b652eb99ae745d3f0c428d73e3fbca7
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-24 09:59:25 +0400

    the values  are checked is added

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     $("input[type='text']").val("welcome");
     $("#test").html("Heloow How are you ! ")
     $("input[type='checkbox']  ").attr("checked", "checked")
-
+    $('#username').attr("type", "password")
 })
