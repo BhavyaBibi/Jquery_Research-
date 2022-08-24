f9788c3071575f3c629ec59916241ea35412b55a
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-24 15:40:56 +0400

    the custom checking is adding

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("input[type='text']").val("welcome");
     $("#test").html("Heloow How are you ! ")
     $("input[type='checkbox']  ").prop("checked", false)
-    $("input[tpe='checkbox']").prop("checked",false)
+    $("input[tpe='checkbox']").prop("checked",true)
     $('#username').attr("type", "password")
     //     $('a').css("color", "red")
     //     $('a').css("background-color", "green")
