dce23827e67d30000e95d8ac5e5837b409869765
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-24 13:00:23 +0400

    then unchecked property is added

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("input[type='text']").val("welcome");
     $("#test").html("Heloow How are you ! ")
-    $("input[type='checkbox']  ").attr("checked", "checked")
+    $("input[type='checkbox']  ").prop("checked", false)
     $('#username').attr("type", "password")
 })
