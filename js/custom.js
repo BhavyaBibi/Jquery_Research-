9a9a9192bcee4b5c32cc63188e9d0cf5dfaec9a0
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-25 16:23:37 +0400

    this method also working

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
-    $("input[type='text']").val("welcome");
-    $("#test").html("Heloow How are you ! ")
-    $("input[type='checkbox']  ").prop("checked", false)
-    $("input[tpe='checkbox']").prop("checked",true)
-    $('#username').attr("type", "password")
+    // $("input[type='text']").val("welcome");
+    // $("#test").html("Heloow How are you ! ")
+    // $("input[type='checkbox']  ").prop("checked", false)
+    // $("input[tpe='checkbox']").prop("checked",true)
+    // $('#username').attr("type", "password")
     //     $('a').css("color", "red")
     //     $('a').css("background-color", "green")
     // })  
 
     //we can set the background color and color 
-    $('a').css({ "color": "white", "background-color": "green", "font-weight": 600 })
+    // $('a').css({ "color": "white", "background-color": "green", "font-weight": 600 })
+    function abc() {
+        alert("welcome")
+    }
+    $('login').click('abc')
+
 })
