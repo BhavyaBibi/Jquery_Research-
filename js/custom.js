60bfedb55a0a68f29a3c2fc0b93e365b100de4c3
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-25 17:55:04 +0400

    KEYPRESSED  event is working in the section

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,34 @@
+// $(document).ready(function () {
+// $("input[type='text']").val("welcome");
+// $("#test").html("Heloow How are you ! ")
+// $("input[type='checkbox']  ").prop("checked", false)
+// $("input[tpe='checkbox']").prop("checked",true)
+// $('#username').attr("type", "password")
+//     $('a').css("color", "red")
+//     $('a').css("background-color", "green")
+// })  
+
+//we can set the background color and color 
+// $('a').css({ "color": "white", "background-color": "green", "font-weight": 600 })
+
+//     $('#login').click(function () {
+//         var uname = $("#username").val()
+//         var pwd = $("#password").val()
+//         if (uname == " ") {
+//             alert("please enter the username")
+//         }
+//         else if (pwd == " ") {
+//             alert("please enter the password")
+//         }
+//         else {
+//             alert("welcome")
+
+//         }
+//     })
+// })
+
 $(document).ready(function () {
-    // $("input[type='text']").val("welcome");
-    // $("#test").html("Heloow How are you ! ")
-    // $("input[type='checkbox']  ").prop("checked", false)
-    // $("input[tpe='checkbox']").prop("checked",true)
-    // $('#username').attr("type", "password")
-    //     $('a').css("color", "red")
-    //     $('a').css("background-color", "green")
-    // })  
-
-    //we can set the background color and color 
-    // $('a').css({ "color": "white", "background-color": "green", "font-weight": 600 })
-
-    $('#login').click(function () {
-        var uname = $("#username").val()
-        var pwd = $("#password").val()
-        if (uname == " ") {
-            alert("please enter the username")
-        }
-        else if (pwd == " ") {
-            alert("please enter the password")
-        }
-        else {
-            alert("welcome")
-
-        }
+    $("#txt").keypress(function () {
+        console.log("keypressed")
     })
 })
-
